5846fb5024449db95475c612b989fb68182354b6
bbogum19 <berkaye2001@hotmail.com>
2022-12-06 12:26:45 +0300

    Create Store_App project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+
+      return(
+        <SafeAreaView>
+          <Text>Store_App projesi olusturuldu</Text>
+        </SafeAreaView>
+      )
+
+}
+
+export default App;
